cd709b295367c6414bbe1807a00de2cb50428d24
Yanagiuchi <2026059@fsc-educ.com>
2026-06-10 11:46:13 +0900

    login画面のget処理追加

--- src/main/java/com/example/blog_app/controller/LoginController.java
+++ src/main/java/com/example/blog_app/controller/LoginController.java
@@ -2,11 +2,17 @@ package com.example.blog_app.controller;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login"; // login.htmlを表示する
+    }
 
     @PostMapping("/login")
     public String login(
@@ -16,7 +22,7 @@ public class LoginController {
 
         // 簡易的なパスワードチェック
         if ("user".equals(username) && "password".equals(password)) {
-            //セッションに「loginUser」という名前でユーザー名を保存
+            // セッションに「loginUser」という名前でユーザー名を保存
             session.setAttribute("loginUser", username);
             return "redirect:/";
         }
